7b978e941e6901f27cb02b7f952bd5c45e2a3fcb
Lucas pereira <lucasgunner3@gmail.com>
2025-02-25 12:16:59 -0300

    feat: criando injeção de deps

---
+++ Movies.Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Movies.Application.Repositories;
+
+namespace Movies.Application
+{
+    public static class ApplicationServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IMoviesRepository, MoviesRepository>();
+            
+            return services;
+        }
+    }
+}
